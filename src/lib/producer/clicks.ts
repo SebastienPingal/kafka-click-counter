06b2139d21899f616d8d8185b54ccc3c86950753
Sebastien <sebastien.pingal@gmail.com>
2025-08-27 14:50:48 +0200

    📝 add spaces for readability

--- src/lib/producer/clicks.ts
+++ src/lib/producer/clicks.ts
@@ -10,12 +10,15 @@ export async function sendClickEvent(event: { event: string }) {
   try {
     await clicksProducer.connect()
     console.log('🕖 connected to clicks producer')
+
     await clicksProducer.send({
       topic: "clicks.events",
       messages: [{ value: JSON.stringify(event) }],
     })
+
     // 🖱️ Click event sent successfully
     console.log("🖱️ Click event sent successfully:", event)
+
   } catch (error) {
     // ⚠️ Failed to send click event
     console.error("⚠️ Failed to send click event:", error)
